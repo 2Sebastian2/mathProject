40f3902ce242aee9b15df06386ab631cce5cfe90
2Sebastian2 <iulian.mirica2603@gmail.com>
2025-04-02 15:24:58 +0200

    Second Commit

--- main.py
+++ main.py
@@ -11,8 +11,16 @@ class XVar:
     def multiply(self, num: int):
         self.multiplier *= num
 
-    def __str__(self):
-        return (f"{abs(self.multiplier)}x^{self.grade} " if self.grade != 1 else f"{abs(self.multiplier)}x ") if self.multiplier != 1 else (f"x^{self.grade} " if self.grade != 1 else f"x ")
+    def __str__(self, init: bool = False):
+        if init:
+            return (f"{abs(self.multiplier)}x^{self.grade}" if self.grade != 1 else f"{abs(self.multiplier)}x") if self.multiplier != 1 else (f"x^{self.grade}" if self.grade != 1 else f"x")
+        else:
+            return (
+                f" {abs(self.multiplier)}x^{self.grade}" if self.grade != 1 else f" {abs(self.multiplier)}x") if self.multiplier != 1 else (f" x^{self.grade}" if self.grade != 1 else f" x")
+
+    def __neg__(self):
+        self.multiplier = -self.multiplier
+        return self
 
 
 # Clase donde se creará la ecuación
@@ -100,31 +108,34 @@ class Equation:
 
     def scramble(self):
         counter = 0
-        while counter < 3:
-            mixList: list = self.left + self.right
-            random.shuffle(mixList)
+        while counter < 10:
+            if random.randint(0, 1) == 0:
+                try:
+                    item = random.choice(self.left)
+                except IndexError:
+                    continue
 
-            self.left.clear()
-            self.right.clear()
+                self.right.append(-item)
+                self.left.remove(item)
+            else:
+                try:
+                    item = random.choice(self.right)
+                except IndexError:
+                    continue
 
-            while len(mixList) > 0:
-                item = random.choice(mixList)
-                if random.randint(0, 2) == 0:
-                    self.left.append(item)
-                else:
-                    self.right.append(item)
-                mixList.remove(item)
+                self.left.append(-item)
+                self.right.remove(item)
 
             counter += 1
 
     def randomEvent(self):
-        match random.randint(0, 4):
+        match random.randint(0, 3):
             case 0:
-                self.sumAndUpdate(random.randint(0, 10), True if random.randint(0, 2) == 1 else False)
+                self.sumAndUpdate(random.randint(1, 9), True if random.randint(0, 1) == 1 else False)
             case 1:
-                self.subtractAndUpdate(random.randint(0, 10), True if random.randint(0, 2) == 1 else False)
+                self.subtractAndUpdate(random.randint(1, 9), True if random.randint(0, 1) == 1 else False)
             case 2:
-                self.multiplyAndUpdate(random.randint(0, 10))
+                self.multiplyAndUpdate(random.randint(1, 9))
             case 3:
                 self.scramble()
 
@@ -134,29 +145,23 @@ class Equation:
         for item in self.left:
             if counter == 0:
                 if isinstance(item, int):
-                    text += f"- {abs(item)} " if item < 0 else f"{item} "
+                    text += f"- {abs(item)}" if item < 0 else f"{item}"
                 elif isinstance(item, XVar):
-                    text += f"- {item} " if item.multiplier < 0 else f"{item} "
+                    text += f"- {item.__str__(True)}" if item.multiplier < 0 else f"{item.__str__(True)}"
             else:
                 if isinstance(item, int):
-                    text += f"- {abs(item)} " if item < 0 else f"+ {item} "
+                    text += f" - {abs(item)}" if item < 0 else f" + {item}"
                 elif isinstance(item, XVar):
-                    text += f"- {item} " if item.multiplier < 0 else f"+ {item} "
+                    text += f" - {item}" if item.multiplier < 0 else f" + {item}"
             counter += 1
 
-        text += "= "
+        text += " ="
         counter = 0
         for item in self.right:
-            if counter == 0:
-                if isinstance(item, int):
-                    text += f"- {abs(item)} " if item < 0 else f"{item} "
-                elif isinstance(item, XVar):
-                    text += f"- {item} " if item.multiplier < 0 else f"{item} "
-            else:
-                if isinstance(item, int):
-                    text += f"- {abs(item)} " if item < 0 else f"+ {item} "
-                elif isinstance(item, XVar):
-                    text += f"- {item} " if item.multiplier < 0 else f"+ {item} "
+            if isinstance(item, int):
+                text += f" - {abs(item)}" if item < 0 else f" + {item}"
+            elif isinstance(item, XVar):
+                text += f" - {item}" if item.multiplier < 0 else f" + {item}"
             counter += 1
 
         return text
@@ -168,10 +173,12 @@ class Main:
     def __init__(self, quantity: int, level: int):
         self.quantity = quantity
         self.level = level
-        self.eq = Equation(level, 1, 5)
-        for i in range(10):
-            self.eq.randomEvent()
-        print(self.eq)
+
+        for i in range(100):
+            self.eq = Equation(level, 1, random.randint(0, 9))
+            for j in range(15):
+                self.eq.randomEvent()
+            print(self.eq)
 
 
 m = Main(3, 1)
